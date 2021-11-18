f9c467b29f2b58b87133225aa78ff40cbf4777bd
Gareth Davis <gdavis@softwareinstitute.com>
2021-11-18 10:08:32 +0000

    this is my first commit of the project

---
+++ src/com/tsi/training/davis/gareth/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.tsi.training.davis.gareth.helloworld;
+
+
+public class HelloWorld
+{
+    private static int age = 7;
+    private static float day = 7.0f;
+    private static char letter = 'G';
+    private static char letter2 = (char)(letter + 16);
+    private static int myNumber = letter * 2;
+
+    public static void main(String[] args)
+    {
+        String world = "Hello World ";
+        System.out.println(world + myNumber+letter2);
+
+    }
+
+}
